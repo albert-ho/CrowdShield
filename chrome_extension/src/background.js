ba5fb99cf084a54922ccfb12e5cca91953484804
AlbertH-Ubuntu <albertho2006@gmail.com>
2017-04-08 21:21:21 -0400

    Added input dialogue box feature to jump to desired user-start-end Twitter mention-feed

--- chrome_extension/src/background.js
+++ chrome_extension/src/background.js
@@ -3,7 +3,18 @@
 // found in the LICENSE file.
 
 
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener((function(tab) {
-  chrome.tabs.executeScript(null, {file: "content_script.js"});
-})());
+// Called when the user clicks on the browser action.s
+chrome.browserAction.onClicked.addListener(function(tab){
+
+  var user = prompt("Enter user name (e.g. barackobama)");
+  var start = prompt("Enter start time in specific format (yyyy-mm-dd)");
+  var end = prompt("Enter end time in specific format (yyyy-mm-dd)");
+
+  var query = "https://twitter.com/search?l=&q=";
+  query += "to:" + user + " since:" + start + " until:" + end;
+  twitterURL = encodeURI(query);
+
+  chrome.tabs.create({url: twitterURL}, function(tab) {
+    chrome.tabs.executeScript(tab.id, {file: "src/inject.js"});
+  });
+});
